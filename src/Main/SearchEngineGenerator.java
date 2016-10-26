d7d4d298332c57b2a0157cf551c02fb9012367f6
Younes-Benhoumich <benhoumich.younes@gmail.com>
2016-10-26 16:04:58 +0200

    MAJ : fichiers inverses et fusion fonctionnent mais prennent beaucoup de temps.

--- src/Main/SearchEngineGenerator.java
+++ src/Main/SearchEngineGenerator.java
@@ -1,7 +1,10 @@
 package Main;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.nio.channels.FileChannel;
 import java.util.TreeMap;
 import java.util.TreeSet;
 import Common.Utils;
@@ -14,6 +17,8 @@ public class SearchEngineGenerator {
 	protected static String textDir = "C:/Users/Younes/Desktop/M2 AIC/TC3 - Recherche et extraction d'informations dans les textes/github/text";
 	protected static String dataDir = "C:/Users/Younes/Desktop/M2 AIC/TC3 - Recherche et extraction d'informations dans les textes/github/data";
 	protected static String invertedFilesDir = "C:/Users/Younes/Desktop/M2 AIC/TC3 - Recherche et extraction d'informations dans les textes/github/invertedFiles";
+	protected static String indexDir = "C:/Users/Younes/Desktop/M2 AIC/TC3 - Recherche et extraction d'informations dans les textes/github/index"; 	// where to store the final invertedFile
+
 
 	//Save inverted files by pack = 1 day 
 	public static void saveInvertedFilesByPacks(File dir, Normalizer normalizer, File outDir) throws IOException {
@@ -42,20 +47,54 @@ public class SearchEngineGenerator {
 
 	}
 	
+	//http://stackoverflow.com/questions/106770/standard-concise-way-to-copy-a-file-in-java
+	public static void copyFile(File sourceFile, File destFile) throws IOException {
+	    if(!destFile.exists()) {
+	        destFile.createNewFile();
+	    }
+
+	    FileChannel source = null;
+	    FileChannel destination = null;
+
+	    try {
+	        source = new FileInputStream(sourceFile).getChannel();
+	        destination = new FileOutputStream(destFile).getChannel();
+	        destination.transferFrom(source, 0, source.size());
+	    }
+	    finally {
+	        if(source != null) {
+	            source.close();
+	        }
+	        if(destination != null) {
+	            destination.close();
+	        }
+	    }
+	}
+	
 	//merge inverted files from dir directory to mergedInvertedFile file
-	//TODO : verifier si on peux fusionner deux fichiers et mettre le resultat dans l'un d'eux 
+	//TODO : J'utilise 2 fichiers car sinon je vais utiliser le mm fichier comme entree et sortie de la fusion ! 
 	public static void mergeManyInvertedFiles(File dir, File mergedInvertedFile) throws IOException{
 		
-		int i;
+		int i = 2;
 		
 		if (dir.isDirectory()) {
 			File[] files = dir.listFiles();
+			// fichier temp pour ne pas utiliser mergedInvertedFile comme entree et sortie de mergeInvertedFiles
+			//  sinon on va ecraser du contenu  
+			File temp = new File(mergedInvertedFile.getParentFile()+"/temp.ind");	
 			Utils.mergeInvertedFiles(files[0], files[1], mergedInvertedFile);
-			for(i=2; i< files.length; i++){
-				
-				Utils.mergeInvertedFiles(mergedInvertedFile, files[i], mergedInvertedFile);
-				System.out.println("Fusion avec "+files[i].getName());
+			//Files.copy( mergedInvertedFile.toPath(), temp.toPath());
+			//copyFile(mergedInvertedFile, temp);
+			while(i<= files.length-2){
+				Utils.mergeInvertedFiles(mergedInvertedFile, files[i], temp);
+				Utils.mergeInvertedFiles(temp, files[i+1], mergedInvertedFile);
+				System.out.println("Fusion avec "+files[i].getName()+" et "+files[i+1].getName());
+				i = i+2;
 			}
+			copyFile(mergedInvertedFile, temp);
+			Utils.mergeInvertedFiles(temp, files[i], mergedInvertedFile);
+			System.out.println("Fusion avec "+files[i].getName());
+
 			
 			
 		}
@@ -78,7 +117,7 @@ public class SearchEngineGenerator {
 		//test mergeManyInvertedFiles
 		if(true){
 			try {
-				String mergedFile = invertedFilesDir +"/index.ind";
+				String mergedFile = indexDir +"/index.ind";
 				mergeManyInvertedFiles(new File(invertedFilesDir), new File(mergedFile));
 				
 			} catch (IOException e) {
