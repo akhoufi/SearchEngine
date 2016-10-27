76a1738ad21041d35848f16c3841a6250e598e39
Younes-Benhoumich <benhoumich.younes@gmail.com>
2016-10-27 13:37:32 +0200

    MAJ des fonctions et premier test de la réponse à une requete

--- src/Main/SearchEngineGenerator.java
+++ src/Main/SearchEngineGenerator.java
@@ -1,11 +1,17 @@
 package Main;
 
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.nio.channels.FileChannel;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Scanner;
 import java.util.Set;
@@ -22,12 +28,18 @@ public class SearchEngineGenerator {
 	protected static String dataDir = "C:/Users/Younes/Desktop/M2 AIC/TC3 - Recherche et extraction d'informations dans les textes/github/data/2015";
 	protected static String invertedFilesDir = "C:/Users/Younes/Desktop/M2 AIC/TC3 - Recherche et extraction d'informations dans les textes/github/invertedFiles";
 	protected static String indexDir = "C:/Users/Younes/Desktop/M2 AIC/TC3 - Recherche et extraction d'informations dans les textes/github/index"; 	// where to store the final invertedFile
-
+	protected static String weightsDir = "C:/Users/Younes/Desktop/M2 AIC/TC3 - Recherche et extraction d'informations dans les textes/github/weights"; 
+	protected static String corpusDir = "C:/Users/Younes/Desktop/M2 AIC/TC3 - Recherche et extraction d'informations dans les textes/github/corpus"; 	
+	protected static String resultsDir = "C:/Users/Younes/Desktop/M2 AIC/TC3 - Recherche et extraction d'informations dans les textes/github/results"; 
 
 	//Save inverted files by pack = 1 day 
 	public static void saveInvertedFilesByPacks(File dir, Normalizer normalizer, File outDir) throws IOException {
 		
 		TreeMap<String, TreeSet<String>> invertedF = new TreeMap<>();
+		
+		if (!outDir.exists()) {
+			outDir.mkdirs();
+		}
 		
 		if(dir.isDirectory()){
 			File[] subDirs = dir.listFiles();
@@ -107,9 +119,9 @@ public class SearchEngineGenerator {
 	
 	//Find pages containing the query keywords and returns their names
 	// A optimiser ulterieurement, ne pas parcourir touuuuut l'index, A faire plus tard si on arrive à avoir une première version du moteur
-	public static Set<String> getPages(String query, File invertedFile) throws FileNotFoundException{
+	public static Set<String> getPages(String query, File invertedFile, Normalizer normalizer ) throws FileNotFoundException{
 		Set<String> pages = new HashSet<>();
-		String[] keyWordsList = query.toLowerCase().split(" ");
+		ArrayList<String> keyWordsList = normalizer.normalize(query.toLowerCase());
 		
 		String line = null;
 		String[] words = null;
@@ -118,7 +130,7 @@ public class SearchEngineGenerator {
 			
 		Scanner sc = new java.util.Scanner(invertedFile);
 		
-		if (sc.hasNext()) {
+		while(sc.hasNext()) {
 			line = sc.nextLine();
 			words = line.split("\t");
 			word = words[0];
@@ -141,9 +153,31 @@ public class SearchEngineGenerator {
 		return pages;
 	}
 	
-	//Set a directory with the pages from the text folder (construction du corpus lié à la requete)
+//	//Set a directory with the pages from the text folder (construction du corpus lié à la requete et transformation de la requete en document)
+//	// indir : le chemin do dossier text 
+//	public static void queryToDoc(String query,  File outDir, Normalizer normalizer ) throws IOException{ 
+//		
+//		if (!outDir.exists()) {
+//			outDir.mkdirs();
+//		}
+//		
+//		// on écrit la requete dans un fichier
+//		try {
+//			FileWriter fw = new FileWriter (new File(outDir.getAbsolutePath()+"/query.txt"));
+//			BufferedWriter bw = new BufferedWriter (fw);
+//			PrintWriter out = new PrintWriter (bw);
+//			// Ecriture des mots			
+//			out.println(query.toLowerCase()); 
+//			out.close();
+//		}
+//		catch (Exception e){
+//			System.out.println(e.toString());
+//		}
+//	}
+	
+	//Set a directory with the pages from the text folder (construction du corpus lié à la requete et transformation de la requete en document)
 	// indir : le chemin do dossier text 
-	public static void setPagesDir(Set<String> pages, File inDir, File outDir) throws IOException{
+	public static void setPagesDir(Set<String> pages, String query, File inDir, File outDir) throws IOException{
 		String pageDir = inDir.getAbsolutePath();
 		String[] nameParts = null;
 		String[] pageDate = null;
@@ -151,16 +185,36 @@ public class SearchEngineGenerator {
 		String month = null;
 		String day = null; 
 		
+		if (!outDir.exists()) {
+			outDir.mkdirs();
+		}
+		
+		// on écrit la requete dans un fichier
+		try {
+			FileWriter fw = new FileWriter (new File(outDir.getAbsolutePath()+"/query.txt"));
+			BufferedWriter bw = new BufferedWriter (fw);
+			PrintWriter out = new PrintWriter (bw);
+			// Ecriture des mots			
+			out.println(query.toLowerCase()); 
+			out.close();
+		}
+		catch (Exception e){
+			System.out.println(e.toString());
+		}
+
+		
+		//On copie le corpus dans le dossier outDir, à cote du document de la requete
 		for(String page : pages){
 			nameParts = page.split("_");
 			pageDate = nameParts[0].split("");
-			year = pageDate[1]+pageDate[2]+pageDate[3]+pageDate[4];
-			month = pageDate[5]+pageDate[6];
-			day = pageDate[7]+pageDate[8];
-			pageDir = inDir.getAbsolutePath()+"/"+year+"/"+month+"/"+day+"/"+page;			
+			year = pageDate[0]+pageDate[1]+pageDate[2]+pageDate[3];
+			month = pageDate[4]+pageDate[5];
+			day = pageDate[6]+pageDate[7];
+			pageDir = inDir.getAbsolutePath()+"/"+month+"/"+day+"/"+page;			
 			copyFile(new File(pageDir), new File(outDir.getAbsolutePath()+"/"+page));
 			
 		}
+		
 	}
 
 	
@@ -168,13 +222,21 @@ public class SearchEngineGenerator {
 	// 	public static void getWeightFiles(File inDir, File outDir, Normalizer normalizer) throws IOException {
 	// deja codé dans Utils.java , c'est pour cela que j'ai créé la fonction setPagesDir()
 	// afin de l'utiliser directement
-		
+	// La requete aura aussi son fichier .poids puisqu'on l'a mis avec les documents du corpus 
+	
+	
+
 	
 	// Classement des pages repondant à la requete par similarite decroissante dans un fichier
-	// Voir comment on peut faire ? c'est quoi la similarité entre requete et page ? 
-	// TODO
-	public static void getSimilarPages(String query, Set<File> fileList, File outFile) throws IOException {
-		
+	// La requete est considere comme un document, son .poids doit se trouver dans le dossier 
+	//   inDir a cote des  .poids des autres pages
+	// 
+	public static void getSimilarPages(File inDir, File outFile) throws IOException {
+		File query = new File(inDir.getAbsolutePath()+"/query.poids");
+		File[] files = inDir.listFiles();
+		Set<File> file_list = new HashSet<File>(Arrays.asList(files));
+		file_list.remove(query); //On supprime query pour ne pas calculer la similarite entre le mm fichier
+		Utils.getSimilarDocuments(query,file_list, outFile);
 		
 	}
 	
@@ -197,7 +259,7 @@ public class SearchEngineGenerator {
 	
 	
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		//test saveInvertedFilesByPacks
 		if(false){
 			try {
@@ -210,7 +272,7 @@ public class SearchEngineGenerator {
 		}
 		
 		//test mergeManyInvertedFiles
-		if(true){
+		if(false){
 			try {
 				String mergedFile = indexDir +"/index.ind";
 				mergeManyInvertedFiles(new File(invertedFilesDir), new File(mergedFile));
@@ -218,6 +280,21 @@ public class SearchEngineGenerator {
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
+		}
+		
+		if(true){
+			// test de la réponse à une requete
+			Normalizer stemmer = new FrenchStemmer();
+			String query = "charlie hebdo";
+			Set<String> pagesList = new HashSet<>();
+			File results = new File(resultsDir+"/results.txt");
+			
+			pagesList = getPages(query, new File(indexDir+"/index.ind"), stemmer);
+			
+			setPagesDir(pagesList, query, new File(textDir), new File(corpusDir));
+			Utils.getWeightFiles( new File(corpusDir),  new File(weightsDir), stemmer);
+			getSimilarPages(new File(weightsDir), results);
+
 		}
 
 		
